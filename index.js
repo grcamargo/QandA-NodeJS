aba5906bbe1ed9d34f58672831b6a27b3dc8214b
Gustavo Camargo <gustavoregis@gmail.com>
2022-09-25 09:04:18 -0300

    Project Complete

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const connection = require('./db/db');
+const Question = require('./db/Question');
+const Answer = require('./db/Answer');
 
 connection
     .authenticate()
@@ -22,7 +24,11 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 app.get('/', (req,res) => {
-    res.render('index');
+    Question.findAll({raw: true, order: [['id','DESC']]}).then(questions => {
+        res.render('index', {
+            questions: questions
+        });
+    });
 });
 
 app.get('/question', (req,res) => {
@@ -34,7 +40,49 @@ app.post('/submit', (req,res) => {
     let title = req.body.title;
     let description = req.body.description;
 
-    res.send(`Title: ${title} \n Description: ${description}`);
+    Question.create({
+        title: title,
+        description: description
+    }).then(() => {
+        res.redirect('/');
+    });
+});
+
+app.get('/pagequestion/:id', (req,res) => {
+    let id = req.params.id;
+    Question.findOne({
+        where: {id: id}
+    }).then(question => {
+        if(question != undefined){
+
+            Answer.findAll({
+                where: {questionId: question.id},
+                order: [
+                    ['id', 'DESC']
+                ]
+            }).then( answers => {
+                res.render('pagequestion',{
+                    question: question,
+                    answers: answers
+                });
+            });
+
+        }else{
+            res.redirect('/');
+        }
+    });
+});
+
+app.post('/submitAns', (req,res) => {
+    let body = req.body.body;
+    let questionId = req.body.question;
+    Answer.create({
+        body: body,
+        questionId: questionId
+
+    }).then(() => {
+        res.redirect('/pagequestion/' + questionId);
+    });
 });
 
 app.listen(PORT, () => {
